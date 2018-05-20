aee65eb72fa8364b421be76eb9366a5cd840072f
Manish-Ghumnani <maniac3493@gmail.com>
2018-05-21 00:21:44 +0800

    login and logut

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -20,4 +20,26 @@ export class AuthService {
     .pipe(map(res => res.json()));
     
   }
+
+  authenticateUser(user): any {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user, {headers :  headers})
+    .pipe(map(res => res.json()));
+  }
+
+  storeUserData(token, user){
+    //angular looks for jwt token in this path - id_token
+    localStorage.setItem('id_token', token);
+    //for local storage we convert to string and then store, so when retrieving we can parse as JSON
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout(){
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
 }
